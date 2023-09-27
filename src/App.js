c0d8168b1be8fa6c29c57e03fb6b328d73cfdb64
Vladimir Blinov <vladimir.a.blinov@gmail.com>
2023-09-27 15:39:02 +0300

    Stylized Header tittle letter-spacing and footer all stylized

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import DayPage from "./components/containers/DayPage";
 import "./App.css";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
     return (
         <div className="wrapper">
             <Header/>
             <DayPage/>
+            <Footer />
         </div>
     );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles/Footer.css"
+
+function Footer() {
+    return (
+        <footer className="footer">
+            &copy; vladimir blinov 2023
+        </footer>
+    );
+}
+
+export default Footer;

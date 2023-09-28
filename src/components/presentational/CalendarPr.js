791c29e56c869fc569ce23b4f4aa7fc695daddc3
Vladimir Blinov <vladimir.a.blinov@gmail.com>
2023-09-28 15:56:31 +0300

    Aligned the month buttons

--- src/components/presentational/CalendarPr.js
+++ src/components/presentational/CalendarPr.js
@@ -3,7 +3,7 @@ import React from "react";
 function CalendarPr({previousMonth, nextMonth, selectedDate, renderCalendar}) {
     return <div className="calendar">
         <h2 className="calendarTitle">Страница дня {selectedDate.toDateString()}</h2>
-        <div>
+        <div className="calendarButtons">
             <button onClick={previousMonth} className="button">&#10232; назад</button>
             <h2 className="dateTitle">{selectedDate.toLocaleString('default', {month: 'long', year: 'numeric'})}</h2>
             <button onClick={nextMonth}  className="button">вперед &#10233;</button>

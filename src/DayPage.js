ef1d6cde28f929bc4429eb1dce223b3ee5ea0479
Vladimir Blinov <vladimir.a.blinov@gmail.com>
2023-09-29 11:19:01 +0300

    Changing the title date to local

--- src/DayPage.js
+++ src/DayPage.js
@@ -65,7 +65,7 @@ function DayPage() {
 
     return (
         <div className="day-page">
-            <h2>Страница дня {selectedDate.toDateString()}</h2>
+            <h2>Страница дня {selectedDate.toLocaleString('ru', {day: 'numeric', month: 'long', year: 'numeric'})}</h2>
             <Calendar
                 // onChange={handleDateChange}
                 entries={entries}

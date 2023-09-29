5f69e8cbe09fe600c6b1b7a4775f454155732743
Vladimir Blinov <vladimir.a.blinov@gmail.com>
2023-09-29 21:41:02 +0300

    Added buttons style

--- src/CalendarPr.js
+++ src/CalendarPr.js
@@ -3,9 +3,9 @@ import React from "react";
 function CalendarPr({previousMonth, nextMonth, selectedDate, renderCalendar}) {
     return <div className="calendar">
         <div className="selectMonth">
-            <button onClick={previousMonth}>&#129152; назад</button>
+            <button onClick={previousMonth} className="button">&#129152; назад</button>
             <h2>{selectedDate.toLocaleString('default', {month: 'long', year: 'numeric'})}</h2>
-            <button onClick={nextMonth}>вперед &#129154;</button>
+            <button onClick={nextMonth} className="button">вперед &#129154;</button>
         </div>
         <div className="days">
             {renderCalendar()}

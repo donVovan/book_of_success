a19a63a31a0bc6e32d030fdbf0ad2198f015d2d8
Vladimir Blinov <vladimir.a.blinov@gmail.com>
2023-10-01 20:04:53 +0300

    Reduced the load on the Calendar component as much as possible

--- src/Calendar.js
+++ src/Calendar.js
@@ -1,9 +1,9 @@
-import React, {useState} from "react";
+import React from "react";
 import "./Calendar.css";
 import CalendarPr from "./CalendarPr";
 
-function Calendar({entries, handleShowEntries, renderList, renderAddElement, previousMonth, nextMonth}) {
-    const [selectedDate, setSelectedDate] = useState(new Date());
+function Calendar({entries, handleShowEntries, renderList, renderAddElement, previousMonth, nextMonth, renderCalendar, selectedDate, setSelectedDate}) {
+    // const [selectedDate, setSelectedDate] = useState(new Date());
 
  /*   function handleChange(date) {
         setSelectedDate(date);
@@ -12,70 +12,7 @@ function Calendar({entries, handleShowEntries, renderList, renderAddElement, pre
 
 
 
-    function renderCalendar() {
 
-        const year = selectedDate.getFullYear();
-        const month = selectedDate.getMonth();
-
-        const firstDay = new Date(year, month, 0).getDay();
-        const lastDate = new Date(year, month + 1, 0).getDate();
-
-        const days = [];
-        let dayCounter = 1;
-
-        days.push(
-            <tr>
-                <th>Пн</th>
-                <th>Вт</th>
-                <th>Ср</th>
-                <th>Чт</th>
-                <th>Пт</th>
-                <th>Сб</th>
-                <th>Вс</th>
-            </tr>
-        );
-
-        for (let week = 0; week < 6; week++) {
-            let weekDays = [];
-
-            for (let day = 0; day < 7; day++) {
-                if ((week === 0 && day < firstDay) || dayCounter > lastDate) {
-                    weekDays.push(<td></td>);
-                } else {
-                    const currentDate = new Date(year, month, dayCounter);
-                    const hasEntries = entries.some((entry) =>
-                        entry.date && entry.date.toDateString() === currentDate.toDateString()
-                    );
-
-                    let dayStyles = dayCounter === selectedDate.getDate() ? 'selected' : '';
-                    if (hasEntries) {
-                        dayStyles += ' highlighted';
-                    }
-
-                    weekDays.push(
-                        <td
-                            key={dayCounter}
-                            className={dayStyles}
-                            onClick={()=> handleShowEntries(currentDate)}
-                        >
-                            {dayCounter}
-                        </td>
-                    );
-
-                    dayCounter++;
-                }
-            }
-
-            days.push(<tr>{weekDays}</tr>);
-                if (dayCounter > lastDate) break;
-            }
-
-        return (
-            <table className="calendar">
-                <tbody>{days}</tbody>
-            </table>
-        );
-    }
 
     return <div>
         <CalendarPr
--- src/DayPage.js
+++ src/DayPage.js
@@ -116,6 +116,71 @@ function DayPage() {
         setSelectedDate(currentDate);
     }
 
+    function renderCalendar() {
+
+        const year = selectedDate.getFullYear();
+        const month = selectedDate.getMonth();
+
+        const firstDay = new Date(year, month, 0).getDay();
+        const lastDate = new Date(year, month + 1, 0).getDate();
+
+        const days = [];
+        let dayCounter = 1;
+
+        days.push(
+            <tr>
+                <th>Пн</th>
+                <th>Вт</th>
+                <th>Ср</th>
+                <th>Чт</th>
+                <th>Пт</th>
+                <th>Сб</th>
+                <th>Вс</th>
+            </tr>
+        );
+
+        for (let week = 0; week < 6; week++) {
+            let weekDays = [];
+
+            for (let day = 0; day < 7; day++) {
+                if ((week === 0 && day < firstDay) || dayCounter > lastDate) {
+                    weekDays.push(<td></td>);
+                } else {
+                    const currentDate = new Date(year, month, dayCounter);
+                    const hasEntries = entries.some((entry) =>
+                        entry.date && entry.date.toDateString() === currentDate.toDateString()
+                    );
+
+                    let dayStyles = dayCounter === selectedDate.getDate() ? 'selected' : '';
+                    if (hasEntries) {
+                        dayStyles += ' highlighted';
+                    }
+
+                    weekDays.push(
+                        <td
+                            key={dayCounter}
+                            className={dayStyles}
+                            onClick={()=> handleShowEntries(currentDate)}
+                        >
+                            {dayCounter}
+                        </td>
+                    );
+
+                    dayCounter++;
+                }
+            }
+
+            days.push(<tr>{weekDays}</tr>);
+            if (dayCounter > lastDate) break;
+        }
+
+        return (
+            <table className="calendar">
+                <tbody>{days}</tbody>
+            </table>
+        );
+    }
+
     return (
         <div className="day-page">
             {renderDayTittle()}
@@ -126,6 +191,9 @@ function DayPage() {
                 handleShowEntries={handleShowEntries}
                 previousMonth={previousMonth}
                 nextMonth={nextMonth}
+                renderCalendar={renderCalendar}
+                selectedDate={selectedDate}
+                setSelectedDate={setSelectedDate}
             />
         </div>
     );
